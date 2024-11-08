bc009ce5fe67b34b531afcb110f613ead3f40a50
ISSac25E <92411691+ISSac25E@users.noreply.github.com>
2024-11-07 22:47:04 -0700

    gitignore update

---
+++ main.asm
@@ -0,0 +1,20 @@
+; MASM Template
+; <your name>
+; <today’s date>
+; Create a template for assembler files.
+.386P
+.model flat
+
+extern   _ExitProcess@4: near
+
+.data
+
+.code
+main PROC near
+_main:
+
+	push	0
+	call	_ExitProcess@4
+
+main ENDP
+END

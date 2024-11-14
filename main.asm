15138fc70cfde1ffec2dfa2f44a1af15a27fc73c
ISSac25E <92411691+ISSac25E@users.noreply.github.com>
2024-11-14 12:00:44 -0700

    minor update
    main.asm setup further

--- main.asm
+++ main.asm
@@ -1,20 +1,30 @@
-; MASM Template
-; <your name>
-; <today’s date>
-; Create a template for assembler files.
+; ToDo List application
+; Amir Gorkovchenko
+; 11-14-2024
+
 .386P
 .model flat
 
-extern   _ExitProcess@4: near
+extern _ExitProcess@4: near
+
+include rtc_esp.inc
+include utility.inc
+
+include readWrite.inc
+include linkedList.inc
 
 .data
+toDo_ll_obj dword 0
 
 .code
-main PROC near
+main proc near
 _main:
 
 	push	0
 	call	_ExitProcess@4
 
 main ENDP
+
+print_instructions proc near
+print_instructions endp
 END

32fa52552713633178dd2bc36eb4e83ae2290a4a
ISSac25E <92411691+ISSac25E@users.noreply.github.com>
2024-11-14 18:24:02 -0700

    impl main
    begin implementing main sequence. Just printing simple commands and program greetings

--- main.asm
+++ main.asm
@@ -18,13 +18,36 @@ toDo_ll_obj dword 0
 
 .code
 main proc near
-_main:
+rtc_esp_fail
+rtc_esp_start
+    call initialize_console@0
 
+    print_array_b 3ch, 3ch, 3ch ; "<<<"
+    print_str " Command Line ToDo List "
+    print_array_b 3eh, 3eh, 3eh ; ">>>"
+    println_str
+    println_str
+
+    call print_instructions@0
+
+
+rtc_esp_end
 	push	0
 	call	_ExitProcess@4
 
 main ENDP
 
-print_instructions proc near
-print_instructions endp
+print_instructions@0 proc near
+    ; Please enter a symbol (+, -, ? or !) followed with your To-Do list item
+    ; <+> Add Chores
+    ; <-> Delete Chores
+    ; <?> View List
+    ; <!> End Program
+
+    print_str "Please enter a symbol (+, -, ? or "
+    print_array_b 21h ; '!'
+    println_str ") followed with your To-Do list item"
+    
+    ret
+print_instructions@0 endp
 END

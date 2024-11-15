cb53abda484690d4254354bb719a39c3e99e06e8
ISSac25E <92411691+ISSac25E@users.noreply.github.com>
2024-11-14 19:42:18 -0700

    impl+test clearConsole
    implemented and tested clear console

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-; ToDo List application
+; clear console impl    
 ; Amir Gorkovchenko
 ; 11-14-2024
 
@@ -14,40 +14,18 @@ include readWrite.inc
 include linkedList.inc
 
 .data
-toDo_ll_obj dword 0
 
 .code
-main proc near
-rtc_esp_fail
-rtc_esp_start
+main PROC near
+_main:
+
     call initialize_console@0
 
-    print_array_b 3ch, 3ch, 3ch ; "<<<"
-    print_str " Command Line ToDo List "
-    print_array_b 3eh, 3eh, 3eh ; ">>>"
-    println_str
-    println_str
-
-    call print_instructions@0
-
-
-rtc_esp_end
+    println_str "Hello World"
+    call clearConsole@0
+    
 	push	0
 	call	_ExitProcess@4
 
 main ENDP
-
-print_instructions@0 proc near
-    ; Please enter a symbol (+, -, ? or !) followed with your To-Do list item
-    ; <+> Add Chores
-    ; <-> Delete Chores
-    ; <?> View List
-    ; <!> End Program
-
-    print_str "Please enter a symbol (+, -, ? or "
-    print_array_b 21h ; '!'
-    println_str ") followed with your To-Do list item"
-    
-    ret
-print_instructions@0 endp
 END

7e150e1282b1130189dc943f86f0cde4cfb2bf27
Amir V Gorkovchenko <92411691+ISSac25E@users.noreply.github.com>
2024-11-27 23:39:45 -0700

    dev
    testing done, works good

--- tests/string/main.asm
+++ tests/string/main.asm
@@ -23,10 +23,74 @@ rtc_esp_fail
 rtc_esp_start
     call initialize_console@0
 
-    set_string offset str_1, "Test String"
+    set_string offset str_1
+
+    insert_string offset str_1, "TestString", 0
+
+    insert_string offset str_1, "-", 4
+    insert_string offset str_1, "My ", 0
+
+    push offset str_1
+    call string@length@4
+
+    insert_string offset str_1, " ...", eax
+
+    insert_string offset str_1, " :)", -1
 
     push str_1
     call writeString@4
+    println_str
+
+    push 14 ; high index
+    push 3 ; low index
+    push offset str_1
+    call string@substr@12
+
+    push str_1
+    call writeString@4
+    println_str
+
+    set_string offset str_1, "reverse this"
+
+    push -1 ; high index
+    push 0 ; low index
+    push offset str_1
+    call string@substr@12
+
+    push str_1
+    call writeString@4
+    println_str
+
+
+    push 0 ; high index
+    push -1 ; low index
+    push offset str_1
+    call string@substr@12
+
+    push str_1
+    call writeString@4
+    println_str
+
+    push 10 ; high index
+    push 12 ; low index
+    push offset str_1
+    call string@substr@12
+
+    insert_string offset str_1, " Inserting a random string test for searching: hnbjgk7685, :::;;; test", -1
+
+    push str_1
+    call writeString@4
+    println_str
+
+    strcmp offset str_1, "test", 29
+    
+    print_int eax
+    println_str
+
+    strcmp offset str_1, "test", 30
+    
+    print_int eax
+    println_str
 
 rtc_esp_end
 	push	0
